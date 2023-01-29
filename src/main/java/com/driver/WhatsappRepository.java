683031d970c74376ed0ebefd8c50aa7fe7ab4d2e
zephyr005 <rajkumar.rbj20@gmail.com>
2023-01-29 15:19:40 +0530

    Code3

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -34,7 +34,7 @@ public class WhatsappRepository {
 
     public Group createGroup(List<User> users) {
         if(users.size()==2){
-            Group group = new Group(users.get(1).getName(),0);
+            Group group = new Group(users.get(1).getName(),2);
             groupHashMap.put(group,users);
             return group;
         }
